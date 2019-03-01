eb4a05e4e408b201ae319734c81dcd8945391964
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2019-03-01 17:56:16 -0300

    novo cache

--- public/service-worker-custom.js
+++ public/service-worker-custom.js
@@ -8,10 +8,10 @@ self.addEventListener('install', event => {
 		.then(cache => {
 			return cache.addAll([
 				'/',
-				//'static/js/bundle.js',
-				//'static/js/main.chunk.js',
-				//'static/js/0.chunk.js',
-				//'static/js/1.chunk.js',
+				'static/js/bundle.js',
+				'static/js/main.chunk.js',
+				'static/js/0.chunk.js',
+				'static/js/1.chunk.js',
 				'manifest.json',
 				'favicon.ico',
 				'service-worker-custom.js',
