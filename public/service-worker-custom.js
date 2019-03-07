db57764788dcfe16b738ba6d99fd0ac78e0ed515
Leonardo Pereira Magalhaes <falecomleonardopereira@gmail.com>
2019-03-07 15:52:38 -0300

    custom service 003

--- public/service-worker-custom.js
+++ public/service-worker-custom.js
@@ -1,6 +1,6 @@
 console.log('Custom service worker')
 
-const nomeDoCachePrincipal = 'financeiro-v002'
+const nomeDoCachePrincipal = 'financeiro-v003'
 self.addEventListener('install', event => {
 	console.log('Install')
 	event.waitUntil(
