93e3b688113278d2b730bdd2d6e727b0a1324555
Isaias Sousa de Melo <isaiassdm@hotmail.com>
2019-03-11 19:13:55 -0300

    incluindo layout nas telas de extrato

--- src/components/ExtratoAdministracao.js
+++ src/components/ExtratoAdministracao.js
@@ -2,9 +2,25 @@ import React from 'react'
 import {
 	Row,
 	Col,
+	Card,
+	CardTitle,
+	CardText,
 } from 'reactstrap'
 import { connect } from 'react-redux'
 import { SITUACAO_RECEBIDO, SITUACAO_NAO_RECEBIDO } from '../helpers/constantes'
+import './aux.css';
+
+// ICONS
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faFileAlt, faPowerOff, faQuestionCircle, faBriefcase, faList, faFileInvoiceDollar } from '@fortawesome/free-solid-svg-icons'
+library.add(faUser)
+library.add(faBriefcase)
+library.add(faFileInvoiceDollar)
+library.add(faFileAlt)
+library.add(faPowerOff)
+library.add(faQuestionCircle)
+library.add(faList)
 
 class ExtratoAdministracao extends React.Component {
 
@@ -22,12 +38,87 @@ class ExtratoAdministracao extends React.Component {
 			categorias,
 		} = this.props
 		return (
-			<div>
-				<div style={{backgroundColor: 'red', padding: 10}}>
-					<Row>
-						<Col>
-							Api
+			<div style={{marginTop: 80}}>
+				<div style={{background: '#f9f7f7'}}>
+					<h5 style={{padding: 10, fontWeight: '300', color: '#2f8c7c'}}>Olá, Diego Kort!</h5>
+				
+					<Row style={{justifyContent: 'center'}}>
+						<Col> 
+							<Card className="card-saldo">
+								<CardTitle style={{color: '#2f8c7c'}}>R$ {saldo}</CardTitle>
+								<CardText style={{fontSize: 12}}>Saldo</CardText>
+							</Card> 
 						</Col>
+						<Col>
+							<Card className="card-saldo">
+								<CardTitle style={{color: 'brown'}}>R$ {naoRecebido}</CardTitle>
+								<CardText style={{fontSize: 12}}>Não Recebido</CardText>
+							</Card>
+						</Col>
+					</Row>
+				</div>	
+
+				<div className="container-menu-administracao">
+					<div style={{borderTop: '1px solid #2f8c7c', paddingTop: 5, paddingBottom: 5}}>
+						<p style={{paddingTop: 18, fontSize: 12, marginBottom: 0, color: '#b1b1b1'}}><u>ACESSO RÁPIDO</u></p>
+					</div>
+					<div>
+						<Row>
+							<Col xs="6" style={{paddingTop: 10}}>
+								<Card className="card-menu">
+								<FontAwesomeIcon icon="briefcase" size="lg" />
+									<h6>Empresas</h6>
+								</Card>
+							</Col>
+							<Col xs="6" style={{paddingTop: 10}}>
+								<Card className="card-menu">
+								<FontAwesomeIcon icon="user" size="lg" />
+									<h6>Usuário</h6>
+								</Card>
+							</Col>
+						</Row>
+
+						<Row>
+
+							<Col xs="6" style={{paddingTop: 10}}>
+								<Card className="card-menu">
+								<FontAwesomeIcon icon="file-alt" size="lg" />
+									<h6>Saldo e Extratos</h6>
+								</Card>
+							</Col>
+
+							<Col xs="6" style={{paddingTop: 10 }}>
+								<Card className="card-menu">
+								<FontAwesomeIcon icon="list" size="lg" />
+									<h6>Categorias</h6>
+								</Card>
+							</Col>
+						</Row>
+						<Row>
+							<Col xs="6" sm="4" style={{paddingTop: 10 }}>
+								<Card className="card-menu">
+								<FontAwesomeIcon icon="file-invoice-dollar" size="lg" />
+									<h6>Lançar</h6>
+								</Card>
+							</Col>
+							<Col xs="6" sm="4" style={{paddingTop: 10 }}>
+								<Card className="card-menu">
+								<FontAwesomeIcon icon="power-off" size="lg" />
+									<h6>Sair</h6>
+								</Card>
+							</Col>
+							<Col sm="4" style={{ paddingTop: 10 }}>
+								<Card className="card-menu">
+								<FontAwesomeIcon icon="question-circle" size="lg" />
+									<h6>Suporte</h6>
+								</Card>
+							</Col>
+						</Row>
+					</div>
+				</div>
+				{/* <div style={{backgroundColor: 'red', padding: 10}}>
+					<Row>
+						<Col> Api </Col>
 						<Col>
 							{
 								this.state.api &&
@@ -41,23 +132,15 @@ class ExtratoAdministracao extends React.Component {
 					</Row>
 				</div>	
 				<div style={{backgroundColor: 'lightblue', padding: 10}}>
-					<Row>
-						<Col>
-							Saldo
-						</Col>
-						<Col>
-							{saldo}
-						</Col>
+					<Row> 
+						<Col> Saldo </Col>
+						<Col> {saldo} </Col>
 					</Row>
 				</div>	
 				<div style={{backgroundColor: 'lightgreen', padding: 10}}>
 					<Row>
-						<Col>
-							Nao Recebido
-						</Col>
-						<Col>
-							{naoRecebido}
-						</Col>
+						<Col> Nao Recebido </Col>
+						<Col> {naoRecebido} </Col>
 					</Row>
 				</div>	
 				<div style={{backgroundColor: 'lightcyan', padding: 20}}>
@@ -71,17 +154,13 @@ class ExtratoAdministracao extends React.Component {
 							categorias.map(categoria => {
 								return (
 									<Row key={categoria.id}>
-										<Col>
-											{categoria.nome}	
-										</Col>
-										<Col>
-											{listaDeNaoRecebidoPorCategorias[categoria.id]}
-										</Col>
+										<Col> {categoria.nome} </Col>
+										<Col> {listaDeNaoRecebidoPorCategorias[categoria.id]} </Col>
 									</Row>
 								)
 							})
 					}
-				</div>	
+				</div>	 */}
 			</div>
 		)
 	}
--- src/components/ExtratoEmpresa.js
+++ src/components/ExtratoEmpresa.js
@@ -2,10 +2,25 @@ import React from 'react'
 import {
 	Row,
 	Col,
+	Card,
+	CardTitle,
+	CardText,
 } from 'reactstrap'
 import { connect } from 'react-redux'
-import Lancamentos from './Lancamentos'
+// import Lancamentos from './Lancamentos'
 import { SITUACAO_RECEBIDO, SITUACAO_NAO_RECEBIDO } from '../helpers/constantes'
+import './aux.css';
+
+// ICONS
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faFileInvoiceDollar, faFileAlt, faPowerOff, faQuestionCircle } from '@fortawesome/free-solid-svg-icons'
+library.add(faUser)
+library.add(faFileInvoiceDollar)
+library.add(faFileAlt)
+library.add(faPowerOff)
+library.add(faQuestionCircle)
+
 
 class ExtratoEmpresa extends React.Component {
 
@@ -19,35 +34,76 @@ class ExtratoEmpresa extends React.Component {
 			naoRecebido,
 		} = this.props
 		return (
-			<div>
-				<div style={{backgroundColor: 'lightblue', padding: 10}}>
-					<Row>
-						<Col>
-							Saldo
+			<div style={{marginTop: 80}}>
+				<div style={{background: '#f9f7f7'}}>
+					<h5 style={{padding: 10, fontWeight: '300', color: '#2f8c7c'}}>Olá, Leonardo Pereira!</h5>
+				
+					<Row style={{justifyContent: 'center'}}>
+						<Col> 
+							<Card className="card-saldo">
+								<CardTitle style={{color: '#2f8c7c'}}>R$ {saldo}</CardTitle>
+								<CardText style={{fontSize: 12}}>Saldo</CardText>
+							</Card> 
 						</Col>
 						<Col>
-							{saldo}
-						</Col>
-					</Row>
-				</div>	
-				<div style={{backgroundColor: 'lightgreen', padding: 10}}>
-					<Row>
-						<Col>
-							Nao Recebido
-						</Col>
-						<Col>
-							{naoRecebido}
+							<Card className="card-saldo">
+								<CardTitle style={{color: 'brown'}}>R$ {naoRecebido}</CardTitle>
+								<CardText style={{fontSize: 12}}>Não Recebido</CardText>
+							</Card>
 						</Col>
 					</Row>
 				</div>	
-				<div style={{backgroundColor: 'lightcyan', padding: 20}}>
+
+				<div className="container-menu-empresa">
+					<div style={{borderTop: '1px solid #2f8c7c', paddingTop: 5, paddingBottom: 5}}>
+						<p style={{paddingTop: 18, fontSize: 12, marginBottom: 0, color: '#b1b1b1'}}><u>ACESSO RÁPIDO</u></p>
+					</div>
+					<div>
+						<Row>
+							<Col xs="6" sm="4" style={{paddingTop: 10}}>
+								<Card className="card-menu">
+								<FontAwesomeIcon icon="user" size="lg" />
+									<h6>Usuário</h6>
+								</Card>
+							</Col>
+							<Col xs="6" sm="4" style={{paddingTop: 10}}>
+								<Card className="card-menu">
+								<FontAwesomeIcon icon="file-invoice-dollar" size="lg" />
+									<h6>Lançar Relatório</h6>
+								</Card>
+							</Col>
+							<Col sm="4" style={{paddingTop: 10}}>
+								<Card className="card-menu">
+								<FontAwesomeIcon icon="file-alt" size="lg" />
+									<h6>Saldo e Extratos</h6>
+								</Card>
+							</Col>
+						</Row>
+
+						<Row style={{justifyContent: 'center'}}>
+						<Col style={{paddingTop: 10 }}>
+								<Card className="card-menu">
+								<FontAwesomeIcon icon="power-off" size="lg" />
+									<h6>Sair</h6>
+								</Card>
+							</Col>
+							<Col style={{paddingTop: 10 }}>
+								<Card className="card-menu">
+								<FontAwesomeIcon icon="question-circle" size="lg" />
+									<h6>Suporte</h6>
+								</Card>
+							</Col>
+						</Row>
+					</div>
+				</div>
+				{/* <div style={{backgroundColor: 'lightcyan', padding: 20}}>
 					<Row>
 						<Col style={{textAlign: 'center', backgroundColor: '#AAA'}}>
 							Lancamentos
 						</Col>
 					</Row>
 					<Lancamentos />
-				</div>	
+				</div>	 */}
 			</div>
 		)
 	}
